efe70ba727ce703147e8fcbe51385a22c7e2bb5a
Sergey Shulga <shul.sergio@gmail.com>
2024-10-22 14:27:53 +0300

    v 1.1 add fetch data + test data output

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
-
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { useEffect } from "react";
+import { fetchPlaces } from "./components/service/Api";
+import Loader from "./components/Loader/Loader";
 
 function App() {
+  const [places, setPlaces] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const { data } = await fetchPlaces();
+        console.log("data 1:");
+        console.log(data);
+        setPlaces(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
+    fetchData();
+  }, []);
 
   return (
     <>
- 
       <h1>Places project</h1>
-   
+      {loading && <Loader />}
+      {error && <p>Error in app</p>}
+      <div>
+        {places.length > 0 &&
+          places.map((item) => <p key={item.id}>{item.name}</p>)}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/service/Api.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const fetchPlaces = async () => {
+  const URL = "https://places-project-db.onrender.com/places";
+
+  try {
+    const { data } = await axios.get(URL);
+    return data;
+  } catch (error) {
+    console.error("Err fetch-", error);
+  }
+};
